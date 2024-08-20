4aba62d32fa4401f42387b6ea4f8dce1ffab6252
Savio09 <declanfortune5@gmail.com>
2024-08-19 23:30:03 -0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
-// serve frontend
+
+// serve frontend application
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
