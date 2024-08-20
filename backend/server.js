a977ea098560a0de495f3b2d38f652e60777c4b7
Savio09 <declanfortune5@gmail.com>
2024-08-19 23:08:36 -0700

    just removed the extensions on the routes in server file

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/goals", require("./routes/goalRoutes.js"));
-app.use("/api/users", require("./routes/userRoutes.js"));
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 // serve frontend
 
 if (process.env.NODE_ENV === "production") {
